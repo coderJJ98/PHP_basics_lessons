f6a997f0a3c73f101ab03861c6e2a6b23de0a347
coderJJ98 <jashjoshi.11098@gmail.com>
2020-08-30 23:10:55 +0530

    this is the file which is used to connect in the icludedocuments.ph

---
+++ extensionofdocuments.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<ul>
+<li><a href="">navi1</a></li>
+<li><a href="">navi2</a></li>
+<li><a href="">navi3</a></li>
+<li><a href="">navi4</a></li>
+</ul>
